e0159726a64dba0d68e1749352a98b07291b757d
DAOU-5736\daou <mrson92@gmail.com>
2017-05-21 22:50:38 +0900

    수정사항 1

--- 클래스수업/1.py
+++ 클래스수업/1.py
@@ -1,32 +1,41 @@
 class Cal(object):
     _history = []
-    def __init__(self,v1,v2):
+
+    def __init__(self, v1, v2):
         self.v1 = v1
         self.v2 = v2
+
     def add(self):
-        result = self.v1+self.v2
+        result = self.v1 + self.v2
         Cal._history.append("add:%d+%d=%d" % (self.v1, self.v2, result))
         return result
+
     def subtract(self):
-        result = self.v1-self.v2
-        Cal._history.append("subtract:%d-%d=%d" % (self.v1,self.v2,result))
+        result = self.v1 - self.v2
+        Cal._history.append("subtract:%d-%d=%d" % (self.v1, self.v2, result))
         return result
+
     @classmethod
     def history(cls):
         for item in Cal._history:
             print(item)
+
+
 class CalMultiply(Cal):
     def mutiply(self):
-         result = self.v1*self.v2
-         Cal._history.append("multiply:%d*%d=%d" % (self.v1,self.v2,result))
-         return result
-class CalDivice(CalMultiply):
-    def devide(self):
-        result = self.v1/self.v2
-        Cal._history.append("devide:%d/%d=%d" % (self.v1,self.v2,result))
+        result = self.v1 * self.v2
+        Cal._history.append("multiply:%d*%d=%d" % (self.v1, self.v2, result))
         return result
 
-c1 = Cal(10,10)
+
+class CalDivice(CalMultiply):
+    def devide(self):
+        result = self.v1 / self.v2
+        Cal._history.append("devide:%d/%d=%d" % (self.v1, self.v2, result))
+        return result
+
+
+c1 = Cal(10, 10)
 print(c1.add())
 print(c1.subtract())
 # c1.v2 = 30
